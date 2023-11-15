8a48f35f40abd78647605135f71b00334a05622b
Abrar Hamim <abrarhamim@Abrars-MacBook-Air.local>
2023-11-15 13:40:18 +0600

    Emoji create

---
+++ EmojiLover/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Abrar Hamim on 15/11/23.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case heart = "❤️"
+    case balloon = "🎈"
+    case confetti = "🎉"
+    case cake = "🎂"
+    case sleep = "😴"
+   
+}
+
+
+struct ContentView: View {
+   @State var selection: Emoji = .heart
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases,id: \.self) { emoji in Text(emoji.rawValue)
+                        
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lover!")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Abrar Hamim on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
